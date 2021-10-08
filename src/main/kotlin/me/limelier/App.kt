4f9ff3492e109e00489bccde066890b22dd5ad7d
limelier <tudor.iacobescu.1@gmail.com>
2021-10-09 01:34:01 +0300

    Temporarily revert back to deprecated method until fix is found

--- src/main/kotlin/me/limelier/App.kt
+++ src/main/kotlin/me/limelier/App.kt
@@ -1,6 +1,7 @@
 package me.limelier
 
 import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.json.JsonMapper
 import io.javalin.Javalin
 import me.limelier.handlers.EventHandler
@@ -10,10 +11,9 @@ import io.javalin.plugin.json.JavalinJackson
 public fun makeApp(config: Config): Javalin = Javalin
     .create { cfg ->
         cfg.jsonMapper(JavalinJackson(
-            JsonMapper
-                .builder()
+            JavalinJackson
+                .defaultMapper()
                 .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
-                .build()
         ))
     }
     .get("/", VerifyHandler(config))
